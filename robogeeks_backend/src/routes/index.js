30e26df44d7a1e77317860de6bed846ae43b6d7a
thecreatorzx <saadyesican@gmail.com>
2024-11-23 22:23:21 +0530

    changed the website accordingly

--- robogeeks_backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Example route
-router.get("/", (req, res) => {
-  res.send("Welcome to the API!");
-});
-
-module.exports = router;
